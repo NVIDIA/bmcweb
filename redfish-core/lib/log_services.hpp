70224a0d2d7749d4f07e1ff8d4fe1989c9a46257
deepaky <deepaky@nvidia.com>
2024-11-17 21:33:24 -0800

    Event log entries are not sorted
    Add config option to enable event log sorting,
    by default its disable.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/4727044
    Signed-off-by:Deepak Yadav deepaky@nvidia.com

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -3210,13 +3210,24 @@ inline void requestRoutesDBusEventLogEntryCollection(App& app)
                 }
                 entriesArray.push_back(thisEntry);
             }
-            std::sort(
-                entriesArray.begin(), entriesArray.end(),
-                [](const nlohmann::json& left, const nlohmann::json& right) {
-                int leftId = std::stoi(left["Id"].get<std::string>());
-                int rightId = std::stoi(right["Id"].get<std::string>());
-                return (leftId < rightId);
-            });
+            if constexpr (BMCWEB_SORT_EVENT_LOG)
+            {
+                std::sort(entriesArray.begin(), entriesArray.end(),
+                          [](const nlohmann::json& left,
+                             const nlohmann::json& right) {
+                    int leftId = std::stoi(left["Id"].get<std::string>());
+                    int rightId = std::stoi(right["Id"].get<std::string>());
+                    return (leftId < rightId);
+                });
+            }
+            else
+            {
+                std::sort(entriesArray.begin(), entriesArray.end(),
+                          [](const nlohmann::json& left,
+                             const nlohmann::json& right) {
+                    return (left["Id"] <= right["Id"]);
+                });
+            }
             asyncResp->res.jsonValue["Members@odata.count"] =
                 entriesArray.size();
             asyncResp->res.jsonValue["Members"] = std::move(entriesArray);
@@ -3864,13 +3875,24 @@ inline void requestRoutesDBusSELLogEntryCollection(App& app)
                     }
                 }
             }
-            std::sort(
-                entriesArray.begin(), entriesArray.end(),
-                [](const nlohmann::json& left, const nlohmann::json& right) {
-                int leftId = std::stoi(left["Id"].get<std::string>());
-                int rightId = std::stoi(right["Id"].get<std::string>());
-                return (leftId < rightId);
-            });
+            if constexpr (BMCWEB_SORT_EVENT_LOG)
+            {
+                std::sort(entriesArray.begin(), entriesArray.end(),
+                          [](const nlohmann::json& left,
+                             const nlohmann::json& right) {
+                    int leftId = std::stoi(left["Id"].get<std::string>());
+                    int rightId = std::stoi(right["Id"].get<std::string>());
+                    return (leftId < rightId);
+                });
+            }
+            else
+            {
+                std::sort(entriesArray.begin(), entriesArray.end(),
+                          [](const nlohmann::json& left,
+                             const nlohmann::json& right) {
+                    return (left["Id"] <= right["Id"]);
+                });
+            }
             asyncResp->res.jsonValue["Members@odata.count"] =
                 entriesArray.size();
         },
