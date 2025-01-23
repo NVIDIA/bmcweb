NVIDIA/bmcweb

commit 2b7e9e6451d5102d4d610c638d3bb3e9dca1328c

Update Submit Test event feature to send test data
Changes Added : Updated the submit test event feature to send test
data as per spec

https://www.dmtf.org/sites/default/files/standards/documents/
DSP2046_2019.1.pdf

Testing :

Tested sending custom test data
and same data received at the listener
Fixes nvbug https://nvbugspro.nvidia.com/bug/5015826
Tested below scenarios
1) Send cmd with empty input parameter and verify the received event.
2) Send cmd with one input parameter for every supported parameters
        defined in Redfish spec (link), and verify.
3) Send cmd with full support parameters, and verify.
4) Send cmd with wrong parameter format, wrong parameter received

redfish-core/include/event_service_manager.hpp
@@ -252,7 +252,40 @@ void parseAdditionalDataForCPER(nlohmann::json::object_t& entry,
                                 const nlohmann::json::object_t& oem,
                                 const AdditionalData& additional,
                                 std::string& origin);
-
+struct TestEvent
+{
+    std::optional<int64_t> eventGroupId;
+    std::optional<std::string> eventId;
+    std::optional<std::string> eventTimestamp;
+    std::optional<std::string> message;
+    std::optional<std::vector<std::string>> messageArgs;
+    std::optional<std::string> messageId;
+    std::optional<std::string> originOfCondition;
+    std::optional<std::string> resolution;
+    std::optional<std::string> severity;
+    // default constructor
+    TestEvent() = default;
+    // default assignment operator
+    TestEvent& operator=(const TestEvent&) = default;
+    // default copy constructor
+    TestEvent(const TestEvent&) = default;
+    // constructor with all the aruments
+    TestEvent(std::optional<int64_t> eventGroupId,
+              std::optional<std::string> eventId,
+              std::optional<std::string> eventTimestamp,
+              std::optional<std::string> message,
+              std::optional<std::vector<std::string>> messageArgs,
+              std::optional<std::string> messageId,
+              std::optional<std::string> originOfCondition,
+              std::optional<std::string> resolution,
+              std::optional<std::string> severity) :
+        eventGroupId(eventGroupId),
+        eventId(eventId), eventTimestamp(eventTimestamp), message(message),
+        messageArgs(messageArgs), messageId(messageId),
+        originOfCondition(originOfCondition), resolution(resolution),
+        severity(severity)
+    {}
+};
 /*
  * Structure for an event which is based on Event v1.7.0 in "Redfish Schema
  * Supplement(DSP0268)".
@@ -279,7 +312,6 @@ class Event
     std::string logEntryId = "";
     std::string satBMCLogEntryUrl = "";
     redfish_bool specificEventExistsInGroup = redfishBoolNa;
-
     // derived properties
     std::string registryPrefix;
     std::string resourceType;
@@ -891,31 +923,67 @@ class Subscription : public persistent_data::UserSubscription
         return true;
     }
 
-    bool sendTestEventLog()
+    bool sendTestEventLog(TestEvent& testEvent)
     {
         nlohmann::json::array_t logEntryArray;
-        nlohmann::json& logEntryJson = logEntryArray.emplace_back();
-        uint64_t memberId = 0;
+        nlohmann::json& logEntryJson =
+            logEntryArray.emplace_back(nlohmann::json::object());
 
-        logEntryJson = {{"EventId", "TestID"},
-                        {"EventType", "Event"},
-                        {"Severity", "OK"},
-                        {"Message", "Generated test event"},
-                        {"MessageId", "OpenBMC.0.2.TestEventLog"},
-                        {"MemberId", memberId},
-                        {"MessageArgs", nlohmann::json::array()},
-                        {"EventTimestamp",
-                         redfish::time_utils::getDateTimeOffsetNow().first},
-                        {"Context", customText}};
+        if (testEvent.eventGroupId)
+        {
+            logEntryJson["EventGroupId"] = *testEvent.eventGroupId;
+        }
 
-        nlohmann::json msg;
+        if (testEvent.eventId)
+        {
+            logEntryJson["EventId"] = *testEvent.eventId;
+        }
+
+        if (testEvent.eventTimestamp)
+        {
+            logEntryJson["EventTimestamp"] = *testEvent.eventTimestamp;
+        }
+
+        if (testEvent.originOfCondition)
+        {
+            logEntryJson["OriginOfCondition"]["@odata.id"] =
+                *testEvent.originOfCondition;
+        }
+        if (testEvent.severity)
+        {
+            logEntryJson["Severity"] = *testEvent.severity;
+        }
+
+        if (testEvent.message)
+        {
+            logEntryJson["Message"] = *testEvent.message;
+        }
+
+        if (testEvent.resolution)
+        {
+            logEntryJson["Resolution"] = *testEvent.resolution;
+        }
+
+        if (testEvent.messageId)
+        {
+            logEntryJson["MessageId"] = *testEvent.messageId;
+        }
+
+        if (testEvent.messageArgs)
+        {
+            logEntryJson["MessageArgs"] = *testEvent.messageArgs;
+        }
+        // MemberId is 0 : since we are sending one event record.
+        logEntryJson["MemberId"] = "0";
+
+        nlohmann::json::object_t msg;
         msg["@odata.type"] = "#Event.v1_4_0.Event";
         msg["Id"] = std::to_string(eventSeqNum);
         msg["Name"] = "Event Log";
         msg["Events"] = logEntryArray;
 
-        std::string strMsg = msg.dump(2, ' ', true,
-                                      nlohmann::json::error_handler_t::replace);
+        std::string strMsg = nlohmann::json(msg).dump(
+            2, ' ', true, nlohmann::json::error_handler_t::replace);
         return sendEvent(std::move(strMsg));
     }
 
@@ -1614,12 +1682,12 @@ class EventServiceManager
         return idList;
     }
 
-    bool sendTestEventLog()
+    bool sendTestEventLog(TestEvent& testEvent)
     {
         for (const auto& it : subscriptionsMap)
         {
             std::shared_ptr<Subscription> entry = it.second;
-            if (!entry->sendTestEventLog())
+            if (!entry->sendTestEventLog(testEvent))
             {
                 return false;
             }

redfish-core/lib/event_service.hpp
@@ -184,7 +184,26 @@ inline void requestRoutesSubmitTestEvent(App& app)
         {
             return;
         }
-        if (!EventServiceManager::getInstance().sendTestEventLog())
+
+        TestEvent testEvent;
+        // clang-format off
+        if (!json_util::readJsonAction(
+                req, asyncResp->res,
+                "EventGroupId", testEvent.eventGroupId,
+                "EventId", testEvent.eventId,
+                "EventTimestamp", testEvent.eventTimestamp,
+                "Message", testEvent.message,
+                "MessageArgs", testEvent.messageArgs,
+                "MessageId", testEvent.messageId,
+                "OriginOfCondition", testEvent.originOfCondition,
+                "Resolution", testEvent.resolution,
+                "Severity", testEvent.severity))
+        {
+            return;
+        }
+        // clang-format on
+
+        if (!EventServiceManager::getInstance().sendTestEventLog(testEvent))
         {
             messages::serviceDisabled(asyncResp->res,
                                       "/redfish/v1/EventService/");

test/redfish-core/include/event_service_manager_test.cpp
@@ -13,6 +13,21 @@
 namespace redfish
 {
 
+static TestEvent createTestEvent()
+{
+    TestEvent testEvent;
+    testEvent.eventGroupId = 1;
+    testEvent.eventId = "dummyEvent";
+    testEvent.eventTimestamp = "2021-01";
+    testEvent.message = "Test Message";
+    testEvent.messageArgs = std::vector<std::string>{"arg1", "arg2"};
+    testEvent.messageId = "Dummy message ID";
+    testEvent.originOfCondition = "/redfish/v1/Chassis/GPU_SXM_1";
+    testEvent.resolution = "custom resolution";
+    testEvent.severity = "whatever";
+    return testEvent;
+}
+
 TEST(EventServiceManager, eventMatchesFilter)
 {
     boost::asio::io_context io;
@@ -97,6 +112,52 @@ TEST(EventServiceManager, eventMatchesFilter)
             sub.originResources.emplace_back("/redfish/v1/Managers/bmc_not");
             EXPECT_FALSE(sub.eventMatchesFilter(event, "Event"));
         }
+    }
+}
+
+TEST(EventServiceManager, submitTestEVent)
+{
+    boost::asio::io_context io;
+    boost::urls::url url;
+
+    {
+        Subscription sub(url, io);
+        TestEvent testEvent;
+        EXPECT_TRUE(sub.sendTestEventLog(testEvent));
+    }
+    {
+        Subscription sub(url, io);
+        TestEvent testEvent;
+        testEvent.eventGroupId = 1;
+        testEvent.eventId = "GPU-RST-RECOMM-EVENT";
+        testEvent.eventTimestamp = "2021-01-01T00:00:00Z";
+        testEvent.message = "Custom Message";
+        testEvent.messageArgs = std::vector<std::string>{"GPU_SXM_1",
+                                                         "Restart Recommended"};
+        testEvent.messageId = "Base.1.13.ResetRecommended";
+        testEvent.originOfCondition = "/redfish/v1/Chassis/GPU_SXM_1";
+        testEvent.resolution =
+            "Reset the GPU at the next service window since the ECC errors are contained";
+        testEvent.severity = "Informational";
+        EXPECT_TRUE(sub.sendTestEventLog(testEvent));
+    }
+    {
+        Subscription sub(url, io);
+        TestEvent testEvent = createTestEvent();
+
+        bool result = sub.sendTestEventLog(testEvent);
+
+        EXPECT_TRUE(result);
+
+        EXPECT_EQ(testEvent.eventGroupId.value(), 1);
+        EXPECT_EQ(testEvent.eventId.value(), "dummyEvent");
+        EXPECT_EQ(testEvent.eventTimestamp.value(), "2021-01");
+        EXPECT_EQ(testEvent.message.value(), "Test Message");
+        EXPECT_EQ(testEvent.messageId.value(), "Dummy message ID");
+        EXPECT_EQ(testEvent.originOfCondition.value(),
+                  "/redfish/v1/Chassis/GPU_SXM_1");
+        EXPECT_EQ(testEvent.resolution.value(), "custom resolution");
+        EXPECT_EQ(testEvent.severity.value(), "whatever");
     }
 }
 } // namespace redfish

commit 775e6bee6aa2898fea3f8e2182592672ad911dfb

Fix last event id
EventId is getting generated very inconsistently dependent on which code
is being called.  By design:

1. bmcweb maintains a running counter for EventId
2. all events sent through event service manager should share this
   counter, and increment it when a unique event is sent.

There exists 3 different submit test event paths through event service
manager today.  One for SubmitTestEvent, One for events generated within
bmcweb, and one for events generated that have an origin of condition.

This commit attempts to fix all of them.

Some of this is a regression injected in:
2b7e9e6451d5102d4d610c638d3bb3e9dca1328c

Fixes Nvbug: https://nvbugspro.nvidia.com/bug/5044235

Change-Id: I7f6d59772339f6f950c8f301591e0ed222915a87
Signed-off-by: Ed Tanous <etanous@nvidia.com>

redfish-core/include/event_service_manager.hpp
@@ -191,7 +191,8 @@ inline void getRegistryAndMessageKey(const std::string& messageID,
     }
 }
 
-inline int formatEventLogEntry(const std::string& logEntryID,
+inline int formatEventLogEntry(uint64_t eventId,
+                               const std::string& /*logEntryID*/,
                                const std::string& messageID,
                                const std::span<std::string_view> messageArgs,
                                std::string timestamp,
@@ -224,7 +225,7 @@ inline int formatEventLogEntry(const std::string& logEntryID,
     }
 
     // Fill in the log entry with the gathered data
-    logEntryJson["EventId"] = logEntryID;
+    logEntryJson["EventId"] = std::to_string(eventId);
     logEntryJson["EventType"] = "Event";
     logEntryJson["Severity"] = message->messageSeverity;
     logEntryJson["Message"] = std::move(msg);
@@ -255,7 +256,6 @@ void parseAdditionalDataForCPER(nlohmann::json::object_t& entry,
 struct TestEvent
 {
     std::optional<int64_t> eventGroupId;
-    std::optional<std::string> eventId;
     std::optional<std::string> eventTimestamp;
     std::optional<std::string> message;
     std::optional<std::vector<std::string>> messageArgs;
@@ -263,28 +263,6 @@ struct TestEvent
     std::optional<std::string> originOfCondition;
     std::optional<std::string> resolution;
     std::optional<std::string> severity;
-    // default constructor
-    TestEvent() = default;
-    // default assignment operator
-    TestEvent& operator=(const TestEvent&) = default;
-    // default copy constructor
-    TestEvent(const TestEvent&) = default;
-    // constructor with all the aruments
-    TestEvent(std::optional<int64_t> eventGroupId,
-              std::optional<std::string> eventId,
-              std::optional<std::string> eventTimestamp,
-              std::optional<std::string> message,
-              std::optional<std::vector<std::string>> messageArgs,
-              std::optional<std::string> messageId,
-              std::optional<std::string> originOfCondition,
-              std::optional<std::string> resolution,
-              std::optional<std::string> severity) :
-        eventGroupId(eventGroupId),
-        eventId(eventId), eventTimestamp(eventTimestamp), message(message),
-        messageArgs(messageArgs), messageId(messageId),
-        originOfCondition(originOfCondition), resolution(resolution),
-        severity(severity)
-    {}
 };
 /*
  * Structure for an event which is based on Event v1.7.0 in "Redfish Schema
@@ -294,11 +272,10 @@ class Event
 {
   public:
     // required properties
-    const std::string messageId;
+    std::string messageId;
     // optional properties
     std::vector<std::string> actions = {};
     int64_t eventGroupId = -1;
-    std::string eventId = "";
     std::string eventTimestamp = "";
     std::string logEntry = "";
     std::string memberId = "";
@@ -440,10 +417,6 @@ class Event
         if (eventGroupId >= 0)
         {
             eventLogEntry["EventGroupId"] = eventGroupId;
-        }
-        if (!eventId.empty())
-        {
-            eventLogEntry["EventId"] = eventId;
         }
         if (!eventTimestamp.empty())
         {
@@ -650,8 +623,8 @@ class Subscription : public persistent_data::UserSubscription
 
         if (sseConn != nullptr)
         {
-            eventSeqNum++;
             sseConn->sendEvent(std::to_string(eventSeqNum), msg);
+            eventSeqNum++;
         }
         return true;
     }
@@ -834,72 +807,8 @@ class Subscription : public persistent_data::UserSubscription
         return true;
     }
 
-    bool sendTestEventLog(TestEvent& testEvent)
-    {
-        nlohmann::json::array_t logEntryArray;
-        nlohmann::json& logEntryJson =
-            logEntryArray.emplace_back(nlohmann::json::object());
-
-        if (testEvent.eventGroupId)
-        {
-            logEntryJson["EventGroupId"] = *testEvent.eventGroupId;
-        }
-
-        if (testEvent.eventId)
-        {
-            logEntryJson["EventId"] = *testEvent.eventId;
-        }
-
-        if (testEvent.eventTimestamp)
-        {
-            logEntryJson["EventTimestamp"] = *testEvent.eventTimestamp;
-        }
-
-        if (testEvent.originOfCondition)
-        {
-            logEntryJson["OriginOfCondition"]["@odata.id"] =
-                *testEvent.originOfCondition;
-        }
-        if (testEvent.severity)
-        {
-            logEntryJson["Severity"] = *testEvent.severity;
-        }
-
-        if (testEvent.message)
-        {
-            logEntryJson["Message"] = *testEvent.message;
-        }
-
-        if (testEvent.resolution)
-        {
-            logEntryJson["Resolution"] = *testEvent.resolution;
-        }
-
-        if (testEvent.messageId)
-        {
-            logEntryJson["MessageId"] = *testEvent.messageId;
-        }
-
-        if (testEvent.messageArgs)
-        {
-            logEntryJson["MessageArgs"] = *testEvent.messageArgs;
-        }
-        // MemberId is 0 : since we are sending one event record.
-        logEntryJson["MemberId"] = "0";
-
-        nlohmann::json::object_t msg;
-        msg["@odata.type"] = "#Event.v1_4_0.Event";
-        msg["Id"] = std::to_string(eventSeqNum);
-        msg["Name"] = "Event Log";
-        msg["Events"] = logEntryArray;
-
-        std::string strMsg = nlohmann::json(msg).dump(
-            2, ' ', true, nlohmann::json::error_handler_t::replace);
-        return sendEvent(std::move(strMsg));
-    }
-
     void filterAndSendEventLogs(
-        const std::vector<EventLogObjectsType>& eventRecords)
+        uint64_t eventId, const std::vector<EventLogObjectsType>& eventRecords)
     {
         nlohmann::json::array_t logEntryArray;
         for (const EventLogObjectsType& logEntry : eventRecords)
@@ -909,12 +818,13 @@ class Subscription : public persistent_data::UserSubscription
 
             nlohmann::json::object_t bmcLogEntry;
             if (event_log::formatEventLogEntry(
-                    logEntry.id, logEntry.messageId, messageArgsView,
+                    eventId, logEntry.id, logEntry.messageId, messageArgsView,
                     logEntry.timestamp, customText, bmcLogEntry) != 0)
             {
                 BMCWEB_LOG_DEBUG("Read eventLog entry failed");
                 continue;
             }
+            eventId++;
 
             if (!eventMatchesFilter(bmcLogEntry, ""))
             {
@@ -932,7 +842,6 @@ class Subscription : public persistent_data::UserSubscription
 
         nlohmann::json msg;
         msg["@odata.type"] = "#Event.v1_4_0.Event";
-        msg["Id"] = std::to_string(eventSeqNum);
         msg["Name"] = "Event Log";
         msg["Events"] = std::move(logEntryArray);
         std::string strMsg = msg.dump(2, ' ', true,
@@ -1061,7 +970,7 @@ class EventServiceManager
 
     struct Event2
     {
-        std::string id;
+        uint64_t id;
         nlohmann::json message;
     };
 
@@ -1489,7 +1398,7 @@ class EventServiceManager
             boost::circular_buffer<Event2>::iterator lastEvent =
                 std::find_if(messages.begin(), messages.end(),
                              [&lastEventId](const Event2& event) {
-                return event.id == lastEventId;
+                return std::to_string(event.id) == lastEventId;
             });
             // Can't find a matching ID
             if (lastEvent == messages.end())
@@ -1595,14 +1504,75 @@ class EventServiceManager
 
     bool sendTestEventLog(TestEvent& testEvent)
     {
+        nlohmann::json::array_t logEntryArray;
+        nlohmann::json& logEntryJson =
+            logEntryArray.emplace_back(nlohmann::json::object());
+
+        if (testEvent.eventGroupId)
+        {
+            logEntryJson["EventGroupId"] = *testEvent.eventGroupId;
+        }
+
+        logEntryJson["EventId"] = std::to_string(eventId);
+        eventId++;
+
+        if (testEvent.eventTimestamp)
+        {
+            logEntryJson["EventTimestamp"] = *testEvent.eventTimestamp;
+        }
+
+        if (testEvent.originOfCondition)
+        {
+            logEntryJson["OriginOfCondition"]["@odata.id"] =
+                *testEvent.originOfCondition;
+        }
+        if (testEvent.severity)
+        {
+            logEntryJson["Severity"] = *testEvent.severity;
+        }
+
+        if (testEvent.message)
+        {
+            logEntryJson["Message"] = *testEvent.message;
+        }
+
+        if (testEvent.resolution)
+        {
+            logEntryJson["Resolution"] = *testEvent.resolution;
+        }
+
+        if (testEvent.messageId)
+        {
+            logEntryJson["MessageId"] = *testEvent.messageId;
+        }
+
+        if (testEvent.messageArgs)
+        {
+            logEntryJson["MessageArgs"] = *testEvent.messageArgs;
+        }
+        // MemberId is 0 : since we are sending one event record.
+        logEntryJson["MemberId"] = "0";
+
+        nlohmann::json::object_t msg;
+        msg["@odata.type"] = "#Event.v1_4_0.Event";
+        msg["Name"] = "Event Log";
+        msg["Events"] = logEntryArray;
+
+        messages.push_back(Event2(eventId, msg));
         for (const auto& it : subscriptionsMap)
         {
             std::shared_ptr<Subscription> entry = it.second;
-            if (!entry->sendTestEventLog(testEvent))
+            if (!entry->eventMatchesFilter(msg, "Event"))
             {
-                return false;
+                BMCWEB_LOG_DEBUG("Filter didn't match");
+                continue;
             }
+
+            std::string strMsg = nlohmann::json(msg).dump(
+                2, ' ', true, nlohmann::json::error_handler_t::replace);
+            entry->sendEvent(std::move(strMsg));
         }
+        eventId++;
         return true;
     }
 
@@ -1612,14 +1582,22 @@ class EventServiceManager
         eventMessage["EventId"] = eventId;
 
         eventMessage["EventTimestamp"] =
-            redfish::time_utils::getDateTimeOffsetNow().first;
+            time_utils::getDateTimeOffsetNow().first;
         eventMessage["OriginOfCondition"] = origin;
 
         // MemberId is 0 : since we are sending one event record.
         eventMessage["MemberId"] = 0;
 
-        messages.push_back(Event2(std::to_string(eventId), eventMessage));
+        nlohmann::json::array_t eventRecord;
+        eventRecord.emplace_back(eventMessage);
 
+        nlohmann::json msg;
+
+        msg["@odata.type"] = "#Event.v1_9_0.Event";
+        msg["Name"] = "Event Log";
+        msg["Events"] = std::move(eventRecord);
+
+        messages.push_back(Event2(eventId, eventMessage));
         for (auto& it : subscriptionsMap)
         {
             std::shared_ptr<Subscription>& entry = it.second;
@@ -1629,21 +1607,11 @@ class EventServiceManager
                 continue;
             }
 
-            nlohmann::json::array_t eventRecord;
-            eventRecord.emplace_back(eventMessage);
-
-            nlohmann::json msgJson;
-
-            msgJson["@odata.type"] = "#Event.v1_4_0.Event";
-            msgJson["Name"] = "Event Log";
-            msgJson["Id"] = eventId;
-            msgJson["Events"] = std::move(eventRecord);
-
-            std::string strMsg = msgJson.dump(
+            std::string strMsg = msg.dump(
                 2, ' ', true, nlohmann::json::error_handler_t::replace);
             entry->sendEvent(std::move(strMsg));
-            eventId++; // increment the eventId
         }
+        eventId++;
     }
 
     /*!
@@ -1658,15 +1626,15 @@ class EventServiceManager
         {
             BMCWEB_LOG_ERROR("Failed to format the event log entry");
         }
-        nlohmann::json eventsArray = nlohmann::json::array();
+        logEntry["EventId"] = std::to_string(eventId);
+        nlohmann::json::array_t eventsArray;
         eventsArray.push_back(logEntry);
         nlohmann::json::object_t msg;
         msg["@odata.type"] = "#Event.v1_9_0.Event";
-        msg["Id"] = std::to_string(eventId);
         msg["Name"] = "Event Log";
-        msg["Events"] = eventsArray;
-        messages.push_back(Event2(std::to_string(eventId), msg));
-        for (const auto& it : this->subscriptionsMap)
+        msg["Events"] = std::move(eventsArray);
+        messages.push_back(Event2(eventId, msg));
+        for (const auto& it : subscriptionsMap)
         {
             std::shared_ptr<Subscription> entry = it.second;
             if (!entry->eventMatchesFilter(logEntry, "Event"))
@@ -1771,9 +1739,10 @@ class EventServiceManager
             std::shared_ptr<Subscription> entry = it.second;
             if (entry->eventFormatType == "Event")
             {
-                entry->filterAndSendEventLogs(eventRecords);
+                entry->filterAndSendEventLogs(eventId, eventRecords);
             }
         }
+        eventId += eventRecords.size();
     }
 
     static void watchRedfishEventLogFile()

redfish-core/lib/event_service.hpp
@@ -185,12 +185,17 @@ inline void requestRoutesSubmitTestEvent(App& app)
             return;
         }
 
+        // From the redfish spec on EventId
+        // A service can ignore this value and replace it with its own.
+        // note that his parameter is intentionally ignored.
+        std::optional<std::string> eventId;
+
         TestEvent testEvent;
         // clang-format off
         if (!json_util::readJsonAction(
                 req, asyncResp->res,
                 "EventGroupId", testEvent.eventGroupId,
-                "EventId", testEvent.eventId,
+                "EventId", eventId,
                 "EventTimestamp", testEvent.eventTimestamp,
                 "Message", testEvent.message,
                 "MessageArgs", testEvent.messageArgs,

test/redfish-core/include/event_service_manager_test.cpp
@@ -17,7 +17,6 @@ static TestEvent createTestEvent()
 {
     TestEvent testEvent;
     testEvent.eventGroupId = 1;
-    testEvent.eventId = "dummyEvent";
     testEvent.eventTimestamp = "2021-01";
     testEvent.message = "Test Message";
     testEvent.messageArgs = std::vector<std::string>{"arg1", "arg2"};
@@ -119,17 +118,14 @@ TEST(EventServiceManager, submitTestEVent)
 {
     boost::asio::io_context io;
     boost::urls::url url;
-
+    EventServiceManager& evt = EventServiceManager::getInstance(&io);
     {
-        Subscription sub(url, io);
         TestEvent testEvent;
-        EXPECT_TRUE(sub.sendTestEventLog(testEvent));
+        EXPECT_TRUE(evt.sendTestEventLog(testEvent));
     }
     {
-        Subscription sub(url, io);
         TestEvent testEvent;
         testEvent.eventGroupId = 1;
-        testEvent.eventId = "GPU-RST-RECOMM-EVENT";
         testEvent.eventTimestamp = "2021-01-01T00:00:00Z";
         testEvent.message = "Custom Message";
         testEvent.messageArgs = std::vector<std::string>{"GPU_SXM_1",
@@ -139,18 +135,16 @@ TEST(EventServiceManager, submitTestEVent)
         testEvent.resolution =
             "Reset the GPU at the next service window since the ECC errors are contained";
         testEvent.severity = "Informational";
-        EXPECT_TRUE(sub.sendTestEventLog(testEvent));
+        EXPECT_TRUE(evt.sendTestEventLog(testEvent));
     }
     {
-        Subscription sub(url, io);
         TestEvent testEvent = createTestEvent();
 
-        bool result = sub.sendTestEventLog(testEvent);
+        bool result = evt.sendTestEventLog(testEvent);
 
         EXPECT_TRUE(result);
 
         EXPECT_EQ(testEvent.eventGroupId.value(), 1);
-        EXPECT_EQ(testEvent.eventId.value(), "dummyEvent");
         EXPECT_EQ(testEvent.eventTimestamp.value(), "2021-01");
         EXPECT_EQ(testEvent.message.value(), "Test Message");
         EXPECT_EQ(testEvent.messageId.value(), "Dummy message ID");