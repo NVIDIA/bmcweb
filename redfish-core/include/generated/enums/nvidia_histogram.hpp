ea23005577d340caf272376ee24f702aa08dba82
Utkarsh <uyadav@nvidia.com>
2025-01-27 10:53:16 +0530

    Schema update
    Aim: Schema update for below features
    - Histogram feature
    - Fabric manager
    - Effective error
    
    Fixes NvBug https://
    signed-off-by: Utkarsh Yadav <uyadav@nvidia.com>

--- redfish-core/include/generated/enums/nvidia_histogram.hpp
+++ redfish-core/include/generated/enums/nvidia_histogram.hpp
@@ -9,12 +9,38 @@ enum class BucketUnits{
     Invalid,
     Watts,
     Counts,
+    HundredthsPercent,
+};
+
+enum class BucketFormat{
+    Invalid,
+    NvU8,
+    NvS8,
+    NvU16,
+    NvS16,
+    NvU32,
+    NvS32,
+    NvU64,
+    NvS64,
 };
 
 NLOHMANN_JSON_SERIALIZE_ENUM(BucketUnits, {
     {BucketUnits::Invalid, "Invalid"},
     {BucketUnits::Watts, "Watts"},
     {BucketUnits::Counts, "Counts"},
+    {BucketUnits::HundredthsPercent, "HundredthsPercent"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(BucketFormat, {
+    {BucketFormat::Invalid, "Invalid"},
+    {BucketFormat::NvU8, "NvU8"},
+    {BucketFormat::NvS8, "NvS8"},
+    {BucketFormat::NvU16, "NvU16"},
+    {BucketFormat::NvS16, "NvS16"},
+    {BucketFormat::NvU32, "NvU32"},
+    {BucketFormat::NvS32, "NvS32"},
+    {BucketFormat::NvU64, "NvU64"},
+    {BucketFormat::NvS64, "NvS64"},
 });
 
 }
---
+++ redfish-core/include/generated/enums/nvidia_power_domain.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <nlohmann/json.hpp>
+
+namespace nvidia_power_domain
+{
+// clang-format off
+
+enum class UnitType{
+    Invalid,
+    Watts,
+};
+
+enum class ComparisonType{
+    Invalid,
+    Above,
+    Below,
+};
+
+NLOHMANN_JSON_SERIALIZE_ENUM(UnitType, {
+    {UnitType::Invalid, "Invalid"},
+    {UnitType::Watts, "Watts"},
+});
+
+NLOHMANN_JSON_SERIALIZE_ENUM(ComparisonType, {
+    {ComparisonType::Invalid, "Invalid"},
+    {ComparisonType::Above, "Above"},
+    {ComparisonType::Below, "Below"},
+});
+
+}
+// clang-format on
