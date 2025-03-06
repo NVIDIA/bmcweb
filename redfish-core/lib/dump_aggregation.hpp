4664c18842769dbea3153fea7aafcb08061c8293
Rohit PAI <ropai@nvidia.com>
2025-03-06 19:05:08 +0530

    Cleanup of unused macros
    removed unused platform bmc id meson
    removed redfish-new-powersubsystem
    
    Tested
    - bmcweb builds and image builds
    
    Fixes JIRA https://upstream-sync-cleanup
    Signed-off-by: Rohit PAI <ropai@nvidia.com>

--- redfish-core/lib/dump_aggregation.hpp
+++ redfish-core/lib/dump_aggregation.hpp
@@ -1,9 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2024 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include "app.hpp"
 #include "async_resp.hpp"
 #include "error_messages.hpp"
-#include "redfish_util.hpp"
+#include "query.hpp"
 #include "redfish_aggregator.hpp"
 
 namespace redfish
@@ -21,11 +38,7 @@ inline void handleSetUpRedfishRoute(
     if (!redfish::setUpRedfishRoute(app, req, asyncResp))
     {
         BMCWEB_LOG_DEBUG("Aggregation is enabled. Forwarding URI");
-        return;
     }
-
-    // Do nothing for the Host BMC because it will not invoke this function
-    return;
 }
 
 inline void requestRoutes(App& app)
