7ba04c6c35d967666b2f919130b8df8cdc4a687a
Harry Sung <harrys@nvidia.com>
2025-05-16 06:49:38 -0700

    Fix internal error when querying RoT Chassis URI
    Reason:
    When querying the RoT Chassis URI, bmcweb goes thru all object paths
    under all xyz.openbmc_project.MCTP.Control.XXX.
    However, there is a corner case that if there is no MCTP endpoints
    under a MCTP service, bmcweb returns an internal error as the EID
    pointer is NULL which is incorrect.
    
    Change:
    Only check if EID is NULL only when that object is an MCTP endpoint
    
    Tested:
    Confirmed that no internal error when querying RoT Chassis URI
    
    Fixes NVBUG https://nvbugspro.nvidia.com/bug/5264066
    
    Signed-off-by: Harry Sung <harrys@nvidia.com>

--- redfish-core/include/utils/chassis_utils.hpp
+++ redfish-core/include/utils/chassis_utils.hpp
@@ -712,6 +712,7 @@ inline void
 
                 for (auto& objectPath : resp)
                 {
+                    bool isMctpEp = false;
                     for (auto& interfaceMap : objectPath.second)
                     {
                         if (interfaceMap.first ==
@@ -730,6 +731,7 @@ inline void
                         if (interfaceMap.first ==
                             "xyz.openbmc_project.MCTP.Endpoint")
                         {
+                            isMctpEp = true;
                             for (auto& propertyMap : interfaceMap.second)
                             {
                                 if (propertyMap.first == "EID")
@@ -748,6 +750,13 @@ inline void
                         }
                     }
 
+                    // only check EID and UUID if it's a MCTP endpoint
+                    if (!isMctpEp)
+                    {
+                        continue;
+                    }
+
+                    // only check EID if it's a MCTP endpoint
                     if (eid == nullptr)
                     {
                         BMCWEB_LOG_DEBUG(
