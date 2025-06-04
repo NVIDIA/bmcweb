a161e049fcccb0542ea39fbe9b145baaa284dbd3
Ed Tanous <etanous@nvidia.com>
2025-06-04 08:49:27 -0700

    Updating NV schemas
    Updating to 303a1af1b8a6f025650832245060e052d8c594b5.
    
    Change-Id: I3085ff00291ee039d5f9db81e5f0c621686cc0f5
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ redfish-core/include/generated/enums/nvidia_psu_redundancy.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <nlohmann/json.hpp>
+
+namespace nvidia_psu_redundancy
+{
+// clang-format off
+
+enum class RedundancyType{
+    Invalid,
+    NoRedundancy,
+    NPlusOne,
+    NPlusN,
+};
+
+NLOHMANN_JSON_SERIALIZE_ENUM(RedundancyType, {
+    {RedundancyType::Invalid, "Invalid"},
+    {RedundancyType::NoRedundancy, "NoRedundancy"},
+    {RedundancyType::NPlusOne, "NPlusOne"},
+    {RedundancyType::NPlusN, "NPlusN"},
+});
+
+}
+// clang-format on
