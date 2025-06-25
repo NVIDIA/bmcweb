15c0def5a4595735312456915a4063b7fcc1b85a
Prithvi Pai <ppai@nvidia.com>
2025-06-25 19:03:42 +0530

    Fix logical issue  on IndicatorLED
    Issue:
    Once BMC comes up patch the `IndicatorLED` with `Lit`
    Perform a `GET` and see the `IndicatorLED` value to be
    `Off`
    
    Fix:
    Use `ledOn` to set the property.
    
    Fixes jira https://upstream-sync-sanity-issue
    
    Signed-off-by: Prithvi Pai <ppai@nvidia.com>

--- redfish-core/lib/led.hpp
+++ redfish-core/lib/led.hpp
@@ -139,7 +139,7 @@ inline void setIndicatorLedState(
                 "xyz.openbmc_project.LED.GroupManager",
                 sdbusplus::message::object_path(
                     "/xyz/openbmc_project/led/groups/enclosure_identify"),
-                "xyz.openbmc_project.Led.Group", "Asserted", ledBlinkng);
+                "xyz.openbmc_project.Led.Group", "Asserted", ledOn);
         });
 }
 
