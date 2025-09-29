a78f848b0cd43201d42a35bca86c72890da0954a
Khang D Nguyen <khangng@os.amperecomputing.com>
2025-09-29 10:10:56 +0700

    account_service: Remove success body on 204 response
    * Cherry-pick from the upstream commit adee9f2e.
    * Here is the original commit message -
    ```
    Currently, PATCH requests updating Password to
    /redfish/v1/AccountService/Accounts/{username} will return non-empty
    body despite the status code is 204 No Content:
    
    ```
    curl -X PATCH --user root:0penBmc -H "Content-Type: application/json" --insecure https://${BMC_IP}/redfish/v1/AccountService/Accounts/user_test01 -d '{"Password": "0penBmc"}'
    ```
    
    ```
    Sep 29 02:45:09 mtmitchell-dcscm bmcwebd[947]: [http_response.hpp:212] 0x1bce918 Response content provided but code was no-content or not_modified, which aren't allowed to have a body for url : "/redfish/v1/AccountService/Accounts/user_test01"
    ```
    
    This fixes the issue by removing the success body.
    
    Tested: PATCH requests now do not create the error log.
    ```
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5657442
    
    Signed-off-by: Khang D Nguyen <khangng@os.amperecomputing.com>
    Signed-off-by: Amy Chang <yahanc@nvidia.com>
    Change-Id: I6a6a6964eed4b9ef591f59b5223cc24b9b82680f

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -1163,7 +1163,6 @@ inline void afterVerifyUserExists(
             persistent_data::SessionStore::getInstance()
                 .removeSessionsByUsernameExceptSession(params.username,
                                                        params.session);
-            messages::success(asyncResp->res);
         }
     }
 
