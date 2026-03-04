49b07ba08de104b2fa2be4c80c9afb450961baf7
Mina Yi <minay@nvidia.com>
2026-03-03 18:31:12 -0800

    extract common D-Bus fd streaming and task progress utilities
    move common code out into a utility file
    
    Tested: SPI read API still works
    
    Fixes jira https://jirasw.nvidia.com/browse/DGXOPENBMC-18012
    
    Signed-off-by: Mina Yi <minay@nvidia.com>

--- redfish-core/lib/nvidia_oem_chassis_spi.hpp
+++ redfish-core/lib/nvidia_oem_chassis_spi.hpp
@@ -18,6 +18,7 @@
 
 #include "dbus_utility.hpp"
 #include "task.hpp"
+#include "utils/dbus_fd_download_utils.hpp"
 #include "utils/nvidia_chassis_util.hpp"
 
 #include <boost/system/error_code.hpp>
@@ -27,124 +28,11 @@ namespace redfish
 namespace nvidia_oem_chassis_spi
 {
 
-inline void afterSpiReadFdFound(
-    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
-    const boost::system::error_code& ec, const sdbusplus::message::unix_fd& fd)
-{
-    if (ec)
-    {
-        if (ec == boost::system::errc::host_unreachable)
-        {
-            BMCWEB_LOG_DEBUG("SPI backend wasn't reachable.  Removed?");
-            asyncResp->res.result(boost::beast::http::status::not_found);
-            return;
-        }
-        messages::internalError(asyncResp->res);
-        return;
-    }
-
-    // Set response headers for binary file download
-    asyncResp->res.addHeader("Content-Type", "application/octet-stream");
-
-    // Send raw binary data
-    lseek(fd, 0, SEEK_SET);
-    asyncResp->res.openFd(dup(fd));
-}
-
-inline void getSpiReadData(const std::string& serviceName,
-                           const sdbusplus::message::object_path& path,
-                           const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
-{
-    BMCWEB_LOG_DEBUG("Getting SPI read data from {} for path {}", serviceName,
-                     path.str);
-    sdbusplus::asio::getProperty<sdbusplus::message::unix_fd>(
-        *crow::connections::systemBus, serviceName, path,
-        "com.nvidia.GraceSPIData", "SpiReadFd",
-        [asyncResp](const boost::system::error_code& ec,
-                    const sdbusplus::message::unix_fd& fd) {
-            afterSpiReadFdFound(asyncResp, ec, fd);
-        });
-}
-
 enum class SpiEventType
 {
     SpiRead,
     SpiErase,
 };
-
-inline bool onSpiEvent(const boost::system::error_code& ec,
-                       sdbusplus::message_t& msg,
-                       const std::shared_ptr<task::TaskData>& taskData)
-{
-    if (ec)
-    {
-        taskData->messages.emplace_back(messages::internalError());
-        return task::completed;
-    }
-
-    std::string iface;
-    dbus::utility::DBusPropertiesMap propertiesChanged;
-    std::vector<std::string> invalidProps;
-
-    msg.read(iface, propertiesChanged, invalidProps);
-
-    if (iface != "xyz.openbmc_project.Common.Progress")
-    {
-        return !task::completed;
-    }
-
-    // We got an update, so extend the timeout
-    taskData->extendTimer(std::chrono::seconds(300));
-
-    const std::string* status = nullptr;
-    const uint8_t* progress = nullptr;
-    if (!sdbusplus::unpackPropertiesNoThrow(
-            redfish::dbus_utils::UnpackErrorPrinter(), propertiesChanged,
-            "Status", status, "Progress", progress))
-    {
-        BMCWEB_LOG_ERROR("Failed to unpack properties.  Wrong type?");
-        taskData->messages.emplace_back(messages::internalError());
-        return !task::completed;
-    }
-
-    if (progress != nullptr)
-    {
-        BMCWEB_LOG_DEBUG("Progress changed to {}", *progress);
-        taskData->percentComplete = *progress;
-    }
-
-    if (status != nullptr)
-    {
-        BMCWEB_LOG_DEBUG("Status changed to {}", *status);
-        if (*status !=
-            "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress")
-        {
-            if (*status ==
-                "xyz.openbmc_project.Common.Progress.OperationStatus.Aborted")
-            {
-                std::string index = std::to_string(taskData->index);
-                taskData->messages.emplace_back(messages::taskAborted(index));
-                taskData->state = "Aborted";
-            }
-            else if (
-                *status ==
-                "xyz.openbmc_project.Common.Progress.OperationStatus.Failed")
-            {
-                taskData->messages.emplace_back(messages::internalError());
-                taskData->state = "Exception";
-            }
-            else
-            {
-                taskData->state = "Completed";
-            }
-
-            taskData->percentComplete = 100;
-            return task::completed;
-        }
-    }
-
-    return !task::completed;
-}
 
 inline void afterSpiEventStarted(
     SpiEventType spiEventType, task::Payload&& payload,
@@ -163,14 +51,16 @@ inline void afterSpiEventStarted(
         eraseObjPath.str, "xyz.openbmc_project.Common.Progress");
 
     std::shared_ptr<task::TaskData> task =
-        task::TaskData::createTask(onSpiEvent, match);
+        task::TaskData::createTask(dbus_fd_utils::onProgressEvent, match);
 
     if (spiEventType == SpiEventType::SpiRead)
     {
         task::TaskResponseCallback callback =
             [eraseObjPath,
              serviceName](const std::shared_ptr<bmcweb::AsyncResp>& aResp) {
-                getSpiReadData(serviceName, eraseObjPath, aResp);
+                dbus_fd_utils::getDbusResultFd(serviceName, eraseObjPath,
+                                               "com.nvidia.GraceSPIData",
+                                               "SpiReadFd", aResp);
             };
         task->taskResponse.emplace<task::TaskResponseCallback>(
             std::move(callback));
