c814b9a7ce64cd98d1dab694aca8efb11fe5b56d
Mina Yi <minay@nvidia.com>
2026-03-03 18:31:12 -0800

    extract common D-Bus fd streaming and task progress utilities
    move common code out into a utility file
    
    Tested: SPI read API still works
    
    Fixes jira https://jirasw.nvidia.com/browse/DGXOPENBMC-18012
    
    Signed-off-by: Mina Yi <minay@nvidia.com>

---
+++ redfish-core/include/utils/dbus_fd_download_utils.hpp
@@ -0,0 +1,146 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2026 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "dbus_utility.hpp"
+#include "task.hpp"
+
+#include <unistd.h>
+
+#include <boost/system/error_code.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <sdbusplus/message/native_types.hpp>
+
+namespace redfish
+{
+namespace dbus_fd_utils
+{
+
+inline void streamFdResponse(
+    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
+    const boost::system::error_code& ec, const sdbusplus::message::unix_fd& fd)
+{
+    if (ec)
+    {
+        if (ec == boost::system::errc::host_unreachable)
+        {
+            BMCWEB_LOG_DEBUG("Backend wasn't reachable");
+            asyncResp->res.result(boost::beast::http::status::not_found);
+            return;
+        }
+        messages::internalError(asyncResp->res);
+        return;
+    }
+
+    asyncResp->res.addHeader("Content-Type", "application/octet-stream");
+
+    lseek(fd, 0, SEEK_SET);
+    asyncResp->res.openFd(dup(fd));
+}
+
+inline void getDbusResultFd(const std::string& serviceName,
+                            const sdbusplus::message::object_path& objectPath,
+                            const std::string& interface,
+                            const std::string& property,
+                            const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    BMCWEB_LOG_DEBUG("Reading result FD from {} path={} iface={} prop={}",
+                     serviceName, objectPath.str, interface, property);
+    sdbusplus::asio::getProperty<sdbusplus::message::unix_fd>(
+        *crow::connections::systemBus, serviceName, objectPath, interface,
+        property,
+        [asyncResp](const boost::system::error_code& ec,
+                    const sdbusplus::message::unix_fd& fd) {
+            streamFdResponse(asyncResp, ec, fd);
+        });
+}
+
+inline bool onProgressEvent(const boost::system::error_code& ec,
+                            sdbusplus::message_t& msg,
+                            const std::shared_ptr<task::TaskData>& taskData)
+{
+    if (ec)
+    {
+        taskData->messages.emplace_back(messages::internalError());
+        return task::completed;
+    }
+
+    std::string iface;
+    dbus::utility::DBusPropertiesMap propertiesChanged;
+    std::vector<std::string> invalidProps;
+
+    msg.read(iface, propertiesChanged, invalidProps);
+
+    if (iface != "xyz.openbmc_project.Common.Progress")
+    {
+        return !task::completed;
+    }
+
+    taskData->extendTimer(std::chrono::seconds(300));
+
+    const std::string* status = nullptr;
+    const uint8_t* progress = nullptr;
+    if (!sdbusplus::unpackPropertiesNoThrow(
+            redfish::dbus_utils::UnpackErrorPrinter(), propertiesChanged,
+            "Status", status, "Progress", progress))
+    {
+        BMCWEB_LOG_ERROR("Failed to unpack properties.  Wrong type?");
+        taskData->messages.emplace_back(messages::internalError());
+        return !task::completed;
+    }
+
+    if (progress != nullptr)
+    {
+        BMCWEB_LOG_DEBUG("Progress changed to {}", *progress);
+        taskData->percentComplete = *progress;
+    }
+
+    if (status != nullptr)
+    {
+        BMCWEB_LOG_DEBUG("Status changed to {}", *status);
+        if (*status !=
+            "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress")
+        {
+            if (*status ==
+                "xyz.openbmc_project.Common.Progress.OperationStatus.Aborted")
+            {
+                std::string index = std::to_string(taskData->index);
+                taskData->messages.emplace_back(messages::taskAborted(index));
+                taskData->state = "Aborted";
+            }
+            else if (
+                *status ==
+                "xyz.openbmc_project.Common.Progress.OperationStatus.Failed")
+            {
+                taskData->messages.emplace_back(messages::internalError());
+                taskData->state = "Exception";
+            }
+            else
+            {
+                taskData->state = "Completed";
+            }
+
+            taskData->percentComplete = 100;
+            return task::completed;
+        }
+    }
+
+    return !task::completed;
+}
+
+} // namespace dbus_fd_utils
+} // namespace redfish
