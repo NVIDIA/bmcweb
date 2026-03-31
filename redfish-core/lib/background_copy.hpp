b7a0a97cc4f0627a0b98d50b00fff9971194fb03
robertpapros <rpapros@nvidia.com>
2026-03-31 14:20:19 +0000

    Improve error handling in updateBackgroundCopyPolicy
    Differentiate between I/O errors and other error types when
    accessing the ImageCopyPolicy interface. Add more precise logging
    and response messages to improve debugging and observability.
    
    Also handle cases where AutomaticBackgroundCopyEnabled is not
    supported for a chassis, ensuring clearer and more accurate error
    reporting.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/6002476
    
    Signed-off-by: Robert Papros <rpapros@nvidia.com>

--- redfish-core/lib/background_copy.hpp
+++ redfish-core/lib/background_copy.hpp
@@ -186,18 +186,29 @@ inline void updateBackgroundCopyPolicy(
     objectPath /= chassisId;
 
     dbus::utility::getDbusObject(
-        objectPath, std::array<std::string_view, 1>{imageCopyPolicyInterface},
-        [asyncResp, chassisId, objectPath,
+        objectPath,
+        std::array<std::string_view, 1>{"com.nvidia.ImageCopyPolicy"},
+        [asyncResp, objectPath,
          enabled](const boost::system::error_code& ec,
                   const dbus::utility::MapperGetObject& object) {
             if (ec)
             {
-                BMCWEB_LOG_ERROR(
-                    "The D-Bus object that implements the ImageCopyPolicy interface at object path '{}' does not exist",
-                    objectPath.str);
-                redfish::messages::resourceErrorsDetectedFormatError(
-                    asyncResp->res, "/redfish/v1/Chassis/" + chassisId,
-                    ec.message());
+                if (ec == boost::system::errc::io_error)
+                {
+                    BMCWEB_LOG_ERROR(
+                        "The D-Bus object that implements the ImageCopyPolicy interface at object path '{}' does not exist; "
+                        "AutomaticBackgroundCopyEnabled is not supported for this chassis",
+                        objectPath.str);
+                    redfish::messages::propertyUnknown(
+                        asyncResp->res, "AutomaticBackgroundCopyEnabled");
+                }
+                else
+                {
+                    BMCWEB_LOG_ERROR(
+                        "ImageCopyPolicy: ObjectMapper GetObject failed for path '{}': {}",
+                        objectPath.str, ec);
+                    redfish::messages::internalError(asyncResp->res);
+                }
                 return;
             }
 
