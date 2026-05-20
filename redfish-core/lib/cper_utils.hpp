8a3c153e38bddc9380b8825b7128e95e2a4b8bbc
Aushim Nagarkatti <anagarkatti@nvidia.com>
2026-05-20 16:37:25 -0700

    SSE/Events: Fix cperSeverityCode handling
    Firstly, do not bail on missing severity code as it is not important to
    decode the Severity field, both on dbus and EventLog.
    
    Fix the SSE path to use MessageSeverity, which is the correct property
    name. This code path isn't executed for EventLog.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/6162672
    
    Signed-off-by: Aushim Nagarkatti <anagarkatti@nvidia.com>

--- redfish-core/lib/cper_utils.hpp
+++ redfish-core/lib/cper_utils.hpp
@@ -211,13 +211,6 @@ inline void parseAdditionalDataForCPER(
     BMCWEB_LOG_DEBUG("Adding notificationType");
     jOut["CPER"]["NotificationType"] = notifT->second;
 
-    const auto& sevCode = additional.find("cperSeverityCode");
-    if (additional.end() == sevCode)
-    {
-        BMCWEB_LOG_ERROR("severity code property not found in CPER log");
-        return;
-    }
-
     const auto& diagData = additional.find("diagnosticData");
     if (additional.end() == diagData)
     {
@@ -387,9 +380,14 @@ inline bool parseCperData(
     additionalData.emplace_back("jsonDiagnosticData", cperJson.dump());
 
     std::string severityStr;
-    if (auto sevIt = evt.find("Severity"); sevIt != evt.end())
+    if (auto sevIt = evt.find("MessageSeverity"); sevIt != evt.end())
     {
-        severityStr = sevIt->get<std::string>();
+        const std::string* messageSeverityPtr =
+            sevIt->get_ptr<const std::string*>();
+        if (messageSeverityPtr != nullptr)
+        {
+            severityStr = *messageSeverityPtr;
+        }
     }
 
     const std::map<std::string, std::string> severityMap = {
