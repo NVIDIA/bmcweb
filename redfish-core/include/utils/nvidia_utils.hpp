098d453716068662f27d95d3b7c8b1fd71748dc1
Ed Tanous <etanous@nvidia.com>
2026-05-29 11:22:43 -0700

    Fix nlohman implicit conversions
    eb6501cb4e69ede2ff7075fcd6c99f146fde98f9 appears to have deleted this
    which causes the build to fail.  Restore it with a version check.
    
    Change-Id: Ibcb8c3846961e986b75b88fa9b55fd5ad9b358de
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- redfish-core/include/utils/nvidia_utils.hpp
+++ redfish-core/include/utils/nvidia_utils.hpp
@@ -30,6 +30,40 @@
 #include <unordered_map>
 #include <vector>
 
+// This is defined in nlohmann master, but not for subprojects.
+#if NLOHMANN_JSON_VERSION_MAJOR >= 3 && NLOHMANN_JSON_VERSION_MINOR >= 11
+namespace nlohmann
+{
+template <typename T>
+struct adl_serializer<std::optional<T>>
+{
+    // NOLINTNEXTLINE(readability-identifier-naming)
+    static void to_json(json& j, const std::optional<T>& opt)
+    {
+        if (opt.has_value())
+        {
+            j = *opt;
+        }
+        else
+        {
+            j = nullptr;
+        }
+    }
+    static void fromJson(const json& j, std::optional<T>& opt)
+    {
+        if (j.is_null())
+        {
+            opt = std::nullopt;
+        }
+        else
+        {
+            opt = j.get<T>();
+        }
+    }
+};
+} // namespace nlohmann
+#endif
+
 namespace nvidia
 {
 namespace nsm_utils
