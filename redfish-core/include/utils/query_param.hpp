68ec59682a30ef22ef34847dfa87736b7767b93e
Juris Polevskis <jpolevskis@nvidia.com>
2026-06-19 15:10:14 +0300

    query_param: propagate session in processOnly
    The $only sub-request was created without the originating session.
    In some cases it caused internal server error 500.
    The fix is to copy the session onto the sub-request, similarly how
    startQuery already does it.
    
    Tested:
    Before:
    ```
    curl -k -u root:<Password> -X GET -H 'Content-Type: application/json' https://<IP>/redfish/v1/AccountService/Accounts?only
    {
      "error": {
        "@Message.ExtendedInfo": [
          {
            "@odata.type": "#Message.v1_1_1.Message",
            "Message": "The request failed due to an internal service error.  The service is still operational.",
            "MessageArgs": [],
            "MessageId": "Base.1.18.1.InternalError",
            "MessageSeverity": "Critical",
            "Resolution": "Resubmit the request.  If the problem persists, consider resetting the service."
          }
        ],
        "code": "Base.1.18.1.InternalError",
        "message": "The request failed due to an internal service error.  The service is still operational."
      }
    }
    ```
    After:
    ```
     curl -k -u root:<Password> -X GET -H 'Content-Type: application/json' https://<IP>/redfish/v1/AccountService/Accounts?only
    {
      "@odata.id": "/redfish/v1/AccountService/Accounts/root",
      "@odata.type": "#ManagerAccount.v1_7_0.ManagerAccount",
      "AccountTypes": [
        "HostConsole",
        "IPMI",
        "Redfish",
        "WebUI",
        "ManagerConsole"
      ],
      "Description": "User Account",
      "Enabled": true,
      "Id": "root",
      "Links": {
        "Role": {
          "@odata.id": "/redfish/v1/AccountService/Roles/Administrator"
        }
      },
      "Locked": false,
      "Locked@Redfish.AllowableValues": [
        "false"
      ],
      "Name": "User Account",
      "Password": null,
      "PasswordChangeRequired": false,
      "RoleId": "Administrator",
      "StrictAccountTypes": true,
      "UserName": "root"
    }
    ```
    
    Fixes nvbug https://redmine.mellanox.com/issues/5059338

--- redfish-core/include/utils/query_param.hpp
+++ redfish-core/include/utils/query_param.hpp
@@ -488,7 +488,8 @@ inline std::optional<Query> parseParameters(boost::urls::params_view urlParams,
 }
 
 inline bool processOnly(crow::App& app, crow::Response& res,
-                        std::function<void(crow::Response&)>& completionHandler)
+                        std::function<void(crow::Response&)>& completionHandler,
+                        const crow::Request& req)
 {
     BMCWEB_LOG_DEBUG("Processing only query param");
     const nlohmann::json::object_t* obj =
@@ -548,8 +549,6 @@ inline bool processOnly(crow::App& app, crow::Response& res,
         completionHandler(res);
         return false;
     }
-    // TODO(Ed) copy request headers?
-    // newReq.session = req.session;
     std::error_code ec;
     auto newReq = std::make_shared<crow::Request>(
         crow::Request::Body{boost::beast::http::verb::get, *url, 11}, ec);
@@ -559,6 +558,8 @@ inline bool processOnly(crow::App& app, crow::Response& res,
         completionHandler(res);
         return false;
     }
+
+    newReq->session = req.session;
 
     auto asyncResp = std::make_shared<bmcweb::AsyncResp>();
     bool needToCallHandlers = false;
@@ -1066,7 +1067,7 @@ inline void processAllParams(
     }
     if (query.isOnly)
     {
-        processOnly(app, intermediateResponse, completionHandler);
+        processOnly(app, intermediateResponse, completionHandler, req);
         return;
     }
 
