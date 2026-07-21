523af5d32ae3f644211b54acbc9597bedfac768e
Dmitrijs Novikovs <dnovikovs@nvidia.com>
2026-07-21 06:26:51 -0700

    fabric: require ConfigureComponents for Switch.Reset action
    Switch.Reset is a destructive action; restrict it to
    redfish::privileges::postSwitch (ConfigureComponents) instead of
    the open Login privilege.
    
    Fixes jira https://redmine.mellanox.com/issues/5059345
    
    Signed-off-by: Dmitrijs Novikovs <dnovikovs@nvidia.com>
    
    
    (cherry picked from commit 9d9515e369694de149388220110cb43609291a3a)
    
    Co-authored-by: Dmitrijs Novikovs <dnovikovs@nvidia.com>

--- redfish-core/lib/fabric.hpp
+++ redfish-core/lib/fabric.hpp
@@ -2245,7 +2245,7 @@ inline void requestRoutesNVSwitchReset(App& app)
 {
     BMCWEB_ROUTE(app, "/redfish/v1/Fabrics/<str>/Switches/<str>/"
                       "Actions/Switch.Reset/")
-        .privileges({{"Login"}})
+        .privileges(redfish::privileges::postSwitch)
         .methods(
             boost::beast::http::verb::
                 post)([&app](
